12aa2df7b6ca3c084ab05099935b860280ee5e1b
Ding <dingma3277@gmail.com>
2019-09-17 14:47:52 -0400

    practice print

---
+++ ex001-010_print/ex001.py
@@ -0,0 +1,61 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+#20190917
+print("Hello World!")
+
+
+# In[2]:
+
+
+print("100 - 25 * 3 % 4 + 31/6 = ", 100 - 25 * 3 % 4 + 31/6)
+print("100 - 25 * 3 % 4 + 31./6 = ", 100 - 25 * 3 % 4 + 31./6)
+print("5 >= -2:", 5 >= -2)
+
+
+# In[3]:
+
+
+car_make = 'Ford'
+car_number = 4
+print("there are", car_number, "cars from", car_make)
+print("there are %d cars from %s." % (car_number, car_make))
+
+
+# In[4]:
+
+
+y = "there are %d cars from %s." % (car_number, car_make)
+print(y)
+w = "This is the left side of..."
+e = "a string with a right side."
+print(w+e)
+
+
+# In[5]:
+
+
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
+
+
+# In[8]:
+
+
+##while True:
+for i in ["/","\\"]:
+    print("%s" % i)
+
+
+# In[ ]:
+
+
+##!jupyter nbconvert --to script ex001.ipynb
+
